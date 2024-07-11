8e265e42c8c26014dc64a18ba5c2ee94996e9db9
Micah Lee <micah@micahflee.com>
2024-07-11 16:50:36 -0700

    Try removing curly braces

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst '${ONION_HOSTNAME} ${APP_HOSTNAME}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$ONION_HOSTNAME $APP_HOSTNAME' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
