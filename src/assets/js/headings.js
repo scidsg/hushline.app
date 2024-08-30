44f58253476f64dd028c017e5a4ebfbd638aaeca
Glenn <git@scidsg.org>
2024-08-30 16:13:32 -0700

    add animated headings

---
+++ src/assets/js/headings.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const words = ["whistleblowers", "students", "workers", "employees", "the public"];
+    const animatedTextElement = document.querySelector(".animated-text");
+    let lastWord = '';
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    function changeWord() {
+        let newWord;
+
+        // Ensure the new word is different from the last word
+        do {
+            shuffle(words);
+            newWord = words[0];
+        } while (newWord === lastWord);
+
+        lastWord = newWord; // Update lastWord with the new word
+
+        animatedTextElement.classList.add("fade-out");
+
+        setTimeout(() => {
+            animatedTextElement.textContent = newWord;
+            animatedTextElement.classList.remove("fade-out");
+        }, 500); // Match this to the CSS transition duration
+    }
+
+    setInterval(changeWord, 4000); // Change every 4 seconds
+});
